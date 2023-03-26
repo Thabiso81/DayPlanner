8b3abeeed5f8579de614b1632fc4c4d475208dda
Thabiso <st10091894@vcconnect.edu.za>
2023-03-26 08:39:19 +0000

    Changed a few things on the recyclerView Viewholder and create task GUI.
    Added some functionality to the CreateTaskActivity.kt activity
    .

--- app/src/main/java/za/edu/varcitycollege/st10091894/dayplanner/CreateTaskActivity.kt
+++ app/src/main/java/za/edu/varcitycollege/st10091894/dayplanner/CreateTaskActivity.kt
@@ -4,17 +4,34 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.create_task)
 
-        val findButton = findViewById<Button>(R.id.bvCreateTask)
-        findButton.setOnClickListener() {
-            val nextPage = Intent(this, MainActivity::class.java)
-            startActivity(nextPage)
-            finish()
+        val createTaskButton:Button = findViewById(R.id.bvCreateTask)
+
+
+        createTaskButton.setOnClickListener() {
+            val taskDescription: TextView = findViewById(R.id.etvTaskDescription)
+            val taskName: TextView = findViewById(R.id.etvTaskName)
+
+            if (taskDescription.getText().toString().isEmpty() || taskName.getText().toString().isEmpty()){
+                Toast.makeText(applicationContext, "Enter all fields.", Toast.LENGTH_LONG).show()
+            } else {
+                val newTask = TaskModel(taskDescription = taskDescription.text.toString(),
+                    taskName = taskName.text.toString())
+
+                TaskList.taskList.add(newTask)
+
+                val nextPage = Intent(this, MainActivity::class.java)
+                startActivity(nextPage)
+                finish()
+            }
         }
     }
 }
--- app/src/main/java/za/edu/varcitycollege/st10091894/dayplanner/TaskList.kt
+++ app/src/main/java/za/edu/varcitycollege/st10091894/dayplanner/TaskList.kt
@@ -1,7 +1,7 @@
 package za.edu.varcitycollege.st10091894.dayplanner
 
 object TaskList {
-    val taskList = listOf<TaskModel>(
+    val taskList = mutableListOf<TaskModel>(
         TaskModel("Walk the dog", "the dog needs a walk"),
         TaskModel("Feed the cat", "the cat will starve if you dont feed it"),
         TaskModel("Do homework", "Maths homework, pages 89-94")
